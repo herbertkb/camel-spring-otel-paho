6b110a8ef2bad245304df37d1ef580dd8814c904
Keith Herbert <kherbert@redhat.com>
2025-03-13 12:58:34 -0400

    save Application.java as prior commit, update pom for jvm21 and use redhat ga for otel agent version

--- camel-spring-otel-paho-producer/src/main/java/com/example/paho/producer/Application.java
+++ camel-spring-otel-paho-producer/src/main/java/com/example/paho/producer/Application.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.amqp.producer;
+package com.example.paho.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
